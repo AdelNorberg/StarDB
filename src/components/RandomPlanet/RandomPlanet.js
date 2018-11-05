ed8053eedcc5daa32a8b60854c3a7e7d39fe63e6
AdelNorberg <darzemanov101@gmail.com>
2018-11-05 08:43:22 +0300

    New init
    Components, services

---
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+import './RandomPlanet.css';
+
+export default class RandomPlanet extends Component {
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
