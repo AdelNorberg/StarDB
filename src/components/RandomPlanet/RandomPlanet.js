f21d432b919af420e10bd6c99e9a785fa4939c17
AdelNorberg <darzemanov101@gmail.com>
2018-11-05 20:35:05 +0300

    Swapi services

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react';
 
+import SwapiServices from '../../services/SwapiServices';
 
 import './RandomPlanet.css';
 
 export default class RandomPlanet extends Component {
 
+  constructor() {
+    super();
+    this.updatePlanet();
+  };
+
+  state = {
+    planet: {}
+  };
+
+  SwapiServices = new SwapiServices();
+
   render() {
+    const { planet: {id, population, rotationPeriod, diameter} } = this.state;
+    
     return (
-      <div></div>
-    )
-  }
+      <div className="random-planet jumbotron rounded">
+        <img className="planet-image"
+             src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+        <div>
+          <h4></h4>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="term">Population</span>
+              <span>{population}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Rotation Period</span>
+              <span>{rotationPeriod}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Diameter</span>
+              <span>{diameter}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+  onPlanetLoaded = (planet) => {
+    this.setState({planet});
+  };
+
+  updatePlanet() {
+    const id = Math.floor(Math.random()*18) + 2;
+    this.SwapiServices.getPlanet(id).then(this.onPlanetLoaded);
+  };
 }
